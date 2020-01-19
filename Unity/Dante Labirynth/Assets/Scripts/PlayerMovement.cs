932b3765a730694c8eafad21af46b668da90a7e0
Mikołaj Stachnik <mikolaj.stachnik@onet.pl>
2020-01-19 23:42:06 +0100

    Movement & Colliders
    Player Movement and Room Collider

---
+++ Unity/Dante Labirynth/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 5f;
+    private Animator anim;
+    private Rigidbody2D rb;
+    Vector2 movement;
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        anim = this.GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Vertical", movement.y);
+        anim.SetFloat("Speed", movement.sqrMagnitude);
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
